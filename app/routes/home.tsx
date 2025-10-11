50d10cfef2197cab628bdcdde39e6d637db3c4dc
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2025-10-11 16:57:13 +0200

    Minor formatting

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,16 @@
 import type { Route } from "./+types/home";
 import { useLoaderData } from "react-router";
-import { getCountryData, getRandomCountryName} from "../scripts/getCountry";
+import { getCountryData, getRandomCountryName } from "../scripts/getCountry";
 import GlobeView from "../components/globeView";
 import { Container } from "@mantine/core";
 import { CountryMenu } from "../components/menu";
-
 
 export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
   const selected = url.searchParams.get("country") ?? getRandomCountryName();
   const countries = ["Norway", "Russia", "Japan"];
   const countryData = await getCountryData(selected);
-  return { countryName: selected, countryData, countries }; 
+  return { countryName: selected, countryData, countries };
 }
 
 export function meta({ data }: Route.MetaArgs) {
@@ -25,7 +24,7 @@ export function meta({ data }: Route.MetaArgs) {
 }
 
 export default function Home() {
-  // destructure exactly what loader returns
+  // destructure loader
   const { countryData, countries, countryName } =
     useLoaderData<typeof loader>();
 
@@ -58,25 +57,27 @@ export default function Home() {
       {/* Globe  + info*/}
       <div className="flex flex-col lg:flex-row w-full min-h-[100vh]">
         <div className="lg:flex-[2] h-[50vh] lg:h-auto relative min-w-0">
-          <GlobeView width="100%" height="100%" countryName={countryName}/>
+          <GlobeView width="100%" height="100%" countryName={countryName} />
         </div>
-         {/* Info box beside it */}
-  <div className="flex-[1] flex flex-col justify-center p-8 bg-gray-900">
-    <h2 className="text-3xl font-bold mb-4">Facts about {countryData.name.common}</h2>
-    <p>Capital: {countryData.capital?.[0]}</p>
-    <p>Population: {countryData.population.toLocaleString()}</p>
-    <p>Region: {countryData.region}</p>
-    {countryData.borders && (
-      <>
-        <h3 className="text-2xl font-semibold mt-6 mb-2">Neighbours:</h3>
-        <ul className="list-disc list-inside">
-          {countryData.borders.map((border: any) => (
-            <li key={border}>{border}</li>
-          ))}
-        </ul>
-      </>
-    )}
-  </div>
+        {/* Info box beside it */}
+        <div className="flex-[1] flex flex-col justify-center p-8 bg-gray-900">
+          <h2 className="text-3xl font-bold mb-4">
+            Facts about {countryData.name.common}
+          </h2>
+          <p>Capital: {countryData.capital?.[0]}</p>
+          <p>Population: {countryData.population.toLocaleString()}</p>
+          <p>Region: {countryData.region}</p>
+          {countryData.borders && (
+            <>
+              <h3 className="text-2xl font-semibold mt-6 mb-2">Neighbours:</h3>
+              <ul className="list-disc list-inside">
+                {countryData.borders.map((border: any) => (
+                  <li key={border}>{border}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
